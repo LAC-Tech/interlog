1f277b83a3d67e53779218644847a7aa6cc0171b
Lewis Campbell <info@lewiscampbell.tech>
2023-12-16 11:36:49 +1300

    reading from log works w/ pwrite

--- src/main.rs
+++ src/main.rs
@@ -12,75 +12,111 @@ use std::io::Read;
 use rand::prelude::*;
 use rustix;
 
-mod replica {
-	use crate::rustix::{fs, io};
-	use crate::Rng;
+#[repr(packed)]
+pub struct ReplicaID([u16; 5]);
+
+impl ReplicaID {
+	fn new<R: Rng>(rng: &mut R) -> Self {
+		Self(rng.gen())
+	}
+}
+
+impl core::fmt::Display for ReplicaID {
+	fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+		for n in self.0 {
+			write!(f, "{:x}", n)?;
+		}
+
+		Ok(())
+	}
+}
+
+#[repr(packed)]
+pub struct LogOffset([u16; 3]);
+
+struct EventID {
+	origin: ReplicaID,
+	offset: LogOffset,
+}
+
+struct EventHeader {
+	origin: ReplicaID,
+	length: [u16; 3], // in bytes, approx 34Gb
+}
+
+// limit rustix to this module?
+mod disk {
+	use crate::rustix::{fd, fs, io};
 	use fs::OFlags;
-	use std::collections::HashMap;
 
-	#[repr(packed)]
-	pub struct ID([u16; 5]);
+	pub struct Log(fd::OwnedFd);
 
-	impl ID {
-		fn new<R: Rng>(rng: &mut R) -> Self {
-			Self(rng.gen())
-		}
-	}
-
-	impl core::fmt::Display for ID {
-		fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
-			for n in self.0 {
-				write!(f, "{:x}", n)?;
-			}
-
-			Ok(())
-		}
-	}
-
-	#[repr(packed)]
-	struct LogPos([u16; 3]);
-
-	struct EventID {
-		origin: ID,
-		log_pos: LogPos,
-	}
-
-	pub struct Local {
-		pub id: ID,
-		pub path: std::path::PathBuf,
-		// Stored in struct so drop will be called, closing the handle
-		fd: rustix::fd::OwnedFd,
-		id_index: HashMap<EventID, LogPos>,
-	}
-
-	impl Local {
-		pub fn new<R: Rng>(rng: &mut R) -> Result<Self, io::Errno> {
-			let id = ID::new(rng);
-			let path_str = format!("/tmp/interlog/{}", id);
-			let path = std::path::PathBuf::from(path_str);
+	impl Log {
+		pub fn new<P: rustix::path::Arg>(path: P) -> Result<Self, io::Errno> {
 			let fd = fs::open(
-				&path,
+				path,
 				OFlags::DIRECT | OFlags::CREATE | OFlags::APPEND | OFlags::RDWR,
 				fs::Mode::RUSR | fs::Mode::WUSR,
 			)?;
-			let id_index = HashMap::new();
-			Ok(Self { id, path, fd, id_index })
+
+			Ok(Self(fd))
 		}
 
-		pub fn write(&self, str: &str) -> Result<usize, io::Errno> {
-			io::write(&self.fd, str.as_bytes())
+		pub fn write(&self, bytes: &[u8]) -> io::Result<usize> {
+			// always sets file offset to EOF.
+			io::write(&self.0, bytes)
+		}
+
+		pub fn read(&self, buf: &mut [u8]) -> io::Result<usize> {
+			// pread ignores the file offset
+			io::pread(&self.0, buf, 0)
 		}
 	}
 }
 
-fn main() {
-	let mut rng = rand::thread_rng();
-	let replica = replica::Local::new(&mut rng).expect("failed to open file");
-	replica.write("Hello,\n").expect("failed to write to replica");
-	replica.write("world!\n").expect("failed to write to replica");
-	println!("Replica with id: {}", replica.id);
-	println!("Press any key to continue");
-	let _ = std::io::stdin().bytes().next();
-	let path = replica.path.clone();
-	std::fs::remove_file(path).expect("failed to remove file");
+pub struct LocalReplica {
+	pub id: ReplicaID,
+	pub path: std::path::PathBuf,
+	log: disk::Log,
+	id_index: Vec<LogOffset>,
+}
+
+impl LocalReplica {
+	pub fn new<R: Rng>(rng: &mut R) -> Result<Self, rustix::io::Errno> {
+		let id = ReplicaID::new(rng);
+		let path_str = format!("/tmp/interlog/{}", id);
+		let path = std::path::PathBuf::from(path_str);
+		let log = disk::Log::new(&path)?;
+		let id_index = vec![];
+		Ok(Self { id, path, log, id_index })
+	}
+
+	pub fn write(&self, str: &str) -> Result<usize, rustix::io::Errno> {
+		self.log.write(str.as_bytes())
+	}
+
+	pub fn read(&self, buf: &mut [u8]) -> Result<usize, rustix::io::Errno> {
+		self.log.read(buf)
+	}
+}
+
+fn main() {}
+
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	#[test]
+	fn read_and_write_to_log() {
+		let mut rng = rand::thread_rng();
+		let replica = LocalReplica::new(&mut rng).expect("failed to open file");
+		replica.write("Hello,\n").expect("failed to write to replica");
+		replica.write("world!\n").expect("failed to write to replica");
+
+		let mut buf = [0; 13];
+		replica.read(&mut buf).expect("failed to read to file");
+		assert_eq!(&buf, b"Hello,\nworld!");
+		let path = replica.path.clone();
+		std::fs::remove_file(path).expect("failed to remove file");
+	}
 }
