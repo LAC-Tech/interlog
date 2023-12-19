f54fe7b1b72b8ba8a7afdb2a85827455ebf1d1f7
Lewis Campbell <info@lewiscampbell.tech>
2023-12-19 14:16:28 +1300

    compressing currently redundant code layers. introduced read cache

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![feature(vec_push_within_capacity)]
+
 #[cfg(not(target_pointer_width = "64"))]
 compile_error!("code assumes 64 bit");
 
@@ -7,12 +9,11 @@ compile_error!("code assumes linux");
 #[cfg(not(target_endian = "little"))]
 compile_error!("code assumes little-endian");
 
-use std::io::Read;
-
-use bytemuck;
+use fs::OFlags;
 use rand::prelude::*;
-use rustix;
+use rustix::{fd, fs, io};
 
+#[derive(Clone, Copy)]
 #[repr(align(2))]
 pub struct ReplicaID([u16; 5]);
 
@@ -32,56 +33,15 @@ impl core::fmt::Display for ReplicaID {
 	}
 }
 
-#[repr(align(2))]
-pub struct LogOffset([u16; 3]);
-
-struct EventID {
-	origin: ReplicaID,
-	offset: LogOffset,
-}
-
-#[repr(align(8))]
-struct EventHeader {
-	length: u32, // in bytes, approx 34Gb
-	_unused: u16,
-	origin: ReplicaID,
-}
-
-// limit rustix to this module?
-mod disk {
-	use crate::rustix::{fd, fs, io};
-	use fs::OFlags;
-
-	pub struct Log(fd::OwnedFd);
-
-	impl Log {
-		pub fn new<P: rustix::path::Arg>(path: P) -> Result<Self, io::Errno> {
-			let fd = fs::open(
-				path,
-				OFlags::DIRECT | OFlags::CREATE | OFlags::APPEND | OFlags::RDWR,
-				fs::Mode::RUSR | fs::Mode::WUSR,
-			)?;
-
-			Ok(Self(fd))
-		}
-
-		pub fn write(&self, bytes: &[u8]) -> io::Result<usize> {
-			// always sets file offset to EOF.
-			io::write(&self.0, bytes)
-		}
-
-		pub fn read(&self, buf: &mut [u8]) -> io::Result<usize> {
-			// pread ignores the file offset
-			io::pread(&self.0, buf, 0)
-		}
-	}
-}
+// Virtual Sector Length
+const VSLEN: usize = 4096;
 
 pub struct LocalReplica {
 	pub id: ReplicaID,
 	pub path: std::path::PathBuf,
-	log: disk::Log,
-	id_index: Vec<LogOffset>,
+	log_fd: fd::OwnedFd,
+	read_cache: Vec<u8>,
+	write_cache: Vec<u8>,
 }
 
 impl LocalReplica {
@@ -89,24 +49,34 @@ impl LocalReplica {
 		let id = ReplicaID::new(rng);
 		let path_str = format!("/tmp/interlog/{}", id);
 		let path = std::path::PathBuf::from(path_str);
-		let log = disk::Log::new(&path)?;
-		let id_index = vec![];
-		Ok(Self { id, path, log, id_index })
+		let log_fd = fs::open(
+			&path,
+			OFlags::DIRECT | OFlags::CREATE | OFlags::APPEND | OFlags::RDWR,
+			fs::Mode::RUSR | fs::Mode::WUSR,
+		)?;
+		let read_cache = Vec::with_capacity(VSLEN);
+		let write_cache = Vec::with_capacity(VSLEN);
+		Ok(Self { id, path, log_fd, read_cache, write_cache })
 	}
 
 	pub fn write(&self, data: &[u8]) -> Result<usize, rustix::io::Errno> {
-		self.log.write(data)
+		// always sets file offset to EOF.
+		io::write(&self.log_fd, data)
 	}
 
-	pub fn read(&self, buf: &mut [u8]) -> Result<usize, rustix::io::Errno> {
-		self.log.read(buf)
+	pub fn read(&mut self) -> Result<&[u8], rustix::io::Errno> {
+		self.read_cache.clear();
+		// pread ignores the file offset
+		io::pread(&self.log_fd, &mut self.read_cache, 0)?;
+		Ok(&self.read_cache)
 	}
 }
 
 fn main() {
 	let mut rng = rand::thread_rng();
 	let rid = ReplicaID::new(&mut rng);
-	let eh = EventHeader { origin: rid, length: 0, _unused: 0 };
+	let mut ns: Vec<u8> = vec![];
+	ns.push_within_capacity(9);
 }
 
 #[cfg(test)]
@@ -117,11 +87,10 @@ mod test {
 	fn read_and_write_to_log() {
 		let mut rng = rand::thread_rng();
 		let replica = LocalReplica::new(&mut rng).expect("failed to open file");
-		replica.write("Hello,\n").expect("failed to write to replica");
-		replica.write("world!\n").expect("failed to write to replica");
+		replica.write(b"Hello,\n").expect("failed to write to replica");
+		replica.write(b"world!\n").expect("failed to write to replica");
 
-		let mut buf = [0; 13];
-		replica.read(&mut buf).expect("failed to read to file");
+		let buf = [0; 13] = replica.read().expect("failed to read to file");
 		assert_eq!(&buf, b"Hello,\nworld!");
 		let path = replica.path.clone();
 		std::fs::remove_file(path).expect("failed to remove file");
