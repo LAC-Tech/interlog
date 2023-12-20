378f27204d7fd69e9ad4131cd49a8168300c49d3
Lewis Campbell <info@lewiscampbell.tech>
2023-12-20 16:48:20 +1300

    introducing concept of eventlent

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ use fs::OFlags;
 use rand::prelude::*;
 use rustix::{fd, fd::AsFd, fs, io};
 
+type O = OFlags;
+
 #[derive(Clone, Copy)]
 #[repr(align(2))]
 pub struct ReplicaID([u16; 5]);
@@ -58,7 +60,7 @@ impl VSect {
 	fn fetch(
 		&mut self,
 		fd: fd::BorrowedFd,
-		len: usize,
+		len: EventLen,
 	) -> rustix::io::Result<usize> {
 		// pread ignores the file offset
 		let bytes_read = io::pread(fd, &mut self.bytes[0..len], 0)?;
@@ -66,21 +68,48 @@ impl VSect {
 		Ok(bytes_read)
 	}
 
-	fn flush(&mut self, fd: fd::BorrowedFd) -> rustix::io::Result<usize> {
+	fn flush(&mut self, fd: fd::BorrowedFd) -> rustix::io::Result<u32> {
 		// always sets file offset to EOF.
 		let bytes_written = io::write(fd, &self.bytes)?;
 		// Resetting
 		self.bytes[0..self.pos].fill(0);
 		self.pos = 0;
-		Ok(bytes_written)
+		Ok(bytes_written.try_into().expect("wrote more than 2^32 bytes"))
 	}
+}
+
+#[repr(transparent)]
+struct EventLen(u8);
+
+impl TryInto<EventLen> for usize {
+	type Error = core::num::TryFromIntError;
+
+	fn try_into(self) -> Result<EventLen, Self::Error> {
+		self.try_into().map(EventLen)
+	}
+}
+
+impl TryInto<usize> for EventLen {
+	type Error = core::convert::Infallible;
+
+	fn try_into(self) -> Result<usize, Self::Error> {
+		self.0.try_into()
+	}
+}
+
+#[repr(align(8))]
+struct EventID {
+	log_offset: u32,
+	len: EventLen,
 }
 
 pub struct LocalReplica {
 	pub id: ReplicaID,
 	pub path: std::path::PathBuf,
 	log_fd: fd::OwnedFd,
+	log_len: u32,
 	write_cache: VSect,
+	id_index: Vec<EventID>,
 }
 
 impl LocalReplica {
@@ -90,22 +119,32 @@ impl LocalReplica {
 		let path = std::path::PathBuf::from(path_str);
 		let log_fd = fs::open(
 			&path,
-			OFlags::DIRECT
-				| OFlags::CREATE | OFlags::APPEND
-				| OFlags::RDWR | OFlags::DSYNC,
+			O::DIRECT | O::CREATE | O::APPEND | O::RDWR | O::DSYNC,
 			fs::Mode::RUSR | fs::Mode::WUSR,
 		)?;
+		let log_len = 0;
 		let write_cache = VSect::new();
-		Ok(Self { id, path, log_fd, write_cache })
+		let id_index = vec![];
+		Ok(Self { id, path, log_fd, log_len, write_cache, id_index })
 	}
 
-	pub fn write(&mut self, data: &[u8]) -> Result<usize, rustix::io::Errno> {
+	pub fn write(&mut self, data: &[u8]) -> Result<(), rustix::io::Errno> {
 		self.write_cache.write(data);
-		self.write_cache.flush(self.log_fd.as_fd())
+		let bytes_written = self.write_cache.flush(self.log_fd.as_fd())?;
+		self.log_len += bytes_written;
+
+		self.id_index.push(EventID {
+			log_offset: self.log_len,
+			len: data
+				.len()
+				.try_into()
+				.expect("Length of event is more than 2^8"),
+		});
+		Ok(())
 	}
 
 	pub fn read(&self, read_cache: &mut VSect) -> rustix::io::Result<usize> {
-		read_cache.fetch(self.log_fd.as_fd())
+		read_cache.fetch(self.log_fd.as_fd(), self.id_index0)
 	}
 }
 
