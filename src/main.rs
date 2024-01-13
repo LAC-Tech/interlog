039196829bea601e8494ef8cce8caded22e1ea16
Lewis Campbell <info@lewiscampbell.tech>
2024-01-13 20:31:59 +1300

    event buffer becomes higher level

--- src/main.rs
+++ src/main.rs
@@ -11,45 +11,72 @@ use fs::OFlags;
 use rand::prelude::*;
 use rustix::{fd, fd::AsFd, fs, io};
 
-// Fixed Capacity Byte Buffers
-struct FCBBuf<const CAPACITY: usize> {
+// 1..N backed by a fixed capacity byte buffer
+// 
+struct EventBuf<const CAPACITY: usize> {
     bytes: Box<[u8; CAPACITY]>,
     len: usize
 }
 
-impl<const CAPACITY: usize> FCBBuf<CAPACITY> {
-    fn new() -> FCBBuf<CAPACITY> {
+impl<const CAPACITY: usize> EventBuf<CAPACITY> {
+    fn new() -> EventBuf<CAPACITY> {
         let bytes = Box::new([0; CAPACITY]);
         let len = 0;
 
         Self {bytes, len}
     }
 
-    fn extend_from_slice(&mut self, other: &[u8]) {
-        let new_len = self.len + other.len();
-        // TODO: proper option type
+    fn write(&mut self, header: &EventHeader, val: &[u8]) {
+        let new_len = EventHeader::LEN + val.len();
         if new_len > CAPACITY { panic!("OVERFLOW") }
-        self.bytes[self.len..new_len].copy_from_slice(other);
+
+        let header = bytemuck::bytes_of(header);
+        self.bytes[self.len..EventHeader::LEN].copy_from_slice(header);
+        self.bytes[self.len + EventHeader::LEN..new_len].copy_from_slice(val);
+
         self.len = new_len;
     }
 
     fn clear(&mut self) {
         self.len = 0;
     }
+
+    // useful for rustix methods, which will fill buffers up to their lengths
+    fn set_len(&mut self, new_len: usize) {
+        if new_len > CAPACITY { panic!("OVERFLOW") }
+        self.len = new_len;
+    }
+
+    /*
+    fn len(&self) -> usize {
+        self.len
+    }
+    */
+    // TODO: make iterator
+    // We assume that 0 is always the start of an event
+    fn as_event(&self) -> Event {
+        let event_header: &EventHeader = 
+            bytemuck::from_bytes(&self.bytes[0..EventHeader::LEN]);
+
+        Event {
+            id: EventID { origin: event_header.origin, pos: 0 },
+            val: &self.bytes[EventID::LEN..self.len]
+        }
+    }
 }
 
-impl<const N: usize> core::ops::Deref for FCBBuf<N> {
+impl<const N: usize> core::ops::Deref for EventBuf<N> {
     type Target = [u8];
 
     #[inline]
     fn deref(&self) -> &[u8] {
-        &*self.bytes
+        &self.bytes[0..self.len]
     }
 }
 
-impl<const N: usize> core::ops::DerefMut for FCBBuf<N> {
+impl<const N: usize> core::ops::DerefMut for EventBuf<N> {
     fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut *self.bytes
+        &mut self.bytes[0..self.len]
     }
 }
 
@@ -60,6 +87,7 @@ type O = OFlags;
 mod size {
     pub const READ_CACHE: usize = 256;
     pub const WRITE_CACHE: usize = 256;
+    pub const EVENT_MAX: usize = 256;
 }
 
 #[derive(bytemuck::Zeroable, bytemuck::Pod, Clone, Copy, Debug)]
@@ -100,47 +128,14 @@ struct Event<'a> { id: EventID, val: &'a [u8] }
 // Including 'len' here to avoid two sys calls when reading event
 struct EventIndex { pos: usize, len: usize }
 
-// TODO: there's some confusion with this struct
-// Is it a buffer? or an immutable collection of events?
-struct EventBuffer { indices: Vec<EventIndex>, bytes: Vec<u8> }
-
-impl EventBuffer {
-    fn new() -> Self {
-        Self { indices: vec![], bytes: vec![] }
-    }
-
-    fn write(&mut self, e: Event) {
-        let index = EventIndex {
-            pos: self.bytes.len(),
-            len: EventID::LEN + e.val.len(),
-        };
-
-        self.bytes.extend_from_slice(bytemuck::bytes_of(&e.id));
-        self.bytes.extend_from_slice(e.val);
-
-        self.indices.push(index);
-    }
-
-    fn read(&self, i: usize) -> Event {
-        let index = &self.indices[i];
-        let id_range = index.pos..index.pos + EventID::LEN;
-        let val_range = index.pos + EventID::LEN..index.len;
-
-        Event {
-            id: *bytemuck::from_bytes(&self.bytes[id_range]),
-            val: &self.bytes[val_range],
-        }
-    }
-}
-
 pub struct LocalReplica {
     pub id: ReplicaID,
     pub path: String,
     log_fd: fd::OwnedFd,
     log_len: usize,
     indices: Vec<EventIndex>,
-    write_cache: FCBBuf<{ size::WRITE_CACHE }>,
-    read_cache: FCBBuf<{ size::READ_CACHE }>
+    write_cache: EventBuf<{ size::WRITE_CACHE }>,
+    read_cache: EventBuf<{ size::READ_CACHE }>
 }
 
 impl LocalReplica {
@@ -152,8 +147,8 @@ impl LocalReplica {
 		let log_fd = Self::open_log(&path)?;
 		let log_len = 0;
         let indices = vec![];
-        let write_cache = FCBBuf::new();
-        let read_cache = FCBBuf::new();
+        let write_cache = EventBuf::new();
+        let read_cache = EventBuf::new();
 		Ok(Self { id, path, log_fd, log_len, indices, write_cache, read_cache })
     }
 
@@ -161,18 +156,19 @@ impl LocalReplica {
     pub fn open(path: &str) -> rustix::io::Result<Self> {
 		let log_fd = Self::open_log(&path)?;
         let mut log_len: usize = 0;
-        let mut read_cache: FCBBuf<{ size::READ_CACHE }> = FCBBuf::new();
+        let mut event_offset = 0;
+        let mut read_cache: EventBuf<{ size::READ_CACHE }> = EventBuf::new();
 
         loop {
-            let bytes_read =
-                io::pread(log_fd.as_fd(), &mut read_cache, log_len as u64)?;
+            let bytes_read = 
+                io::pread(log_fd.as_fd(), &mut read_cache, event_offset)?;
 
             log_len += bytes_read;
 
-            // TODO: read bytes and update indices
-            
+            let event_header: &EventHeader = 
+                bytemuck::from_bytes(&read_cache[0..EventHeader::LEN]);
 
-            if bytes_read > size::READ_CACHE { break }
+            if size::READ_CACHE > bytes_read { break }
         }
 
         panic!("at the disco") 
@@ -187,6 +183,7 @@ impl LocalReplica {
     }
     
     // Event local to the replica, that don't yet have an ID
+    // TODO: write multiple things at a time
     pub fn local_write(&mut self, data: &[u8]) -> rustix::io::Result<()> {
         // Write to cache 
         
@@ -194,8 +191,7 @@ impl LocalReplica {
             len: EventID::LEN + data.len(),
             origin: self.id,
         };
-        self.write_cache.extend_from_slice(bytemuck::bytes_of(&header));
-        self.write_cache.extend_from_slice(data); 
+        self.write_cache.write(&header, data);
 
         // always sets file offset to EOF.
 		let bytes_written = io::write(self.log_fd.as_fd(), &self.write_cache)?;
@@ -214,14 +210,14 @@ impl LocalReplica {
 
 		Ok(())
 	}
-
+    
+    // TODO: try to copy from read cache
     pub fn read(
-        &mut self, buf: &mut Vec<u8>, pos: usize
+        &mut self, buf: &mut EventBuf< {size::EVENT_MAX} >, pos: usize
     ) -> rustix::io::Result<()> {
         let index = &self.indices[pos];
-        unsafe {
-            buf.set_len(index.len)
-        }
+        buf.set_len(index.len);
+
         // pread ignores the fd offset, supply your own
         let bytes_read = io::pread(self.log_fd.as_fd(), buf, index.pos as u64)?;
         
@@ -235,23 +231,6 @@ impl LocalReplica {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn read_and_write_events_onto_buffer() {
-        let val: u64 = 42;
-        let bytes = bytemuck::bytes_of(&val);
-        let e = Event {
-            id: EventID { origin: ReplicaID(0), pos: 0 },
-            val: bytes,
-        };
-
-        let mut es = EventBuffer::new(); 
-
-        es.write(e);
-        let actual = es.read(0);
-
-        assert_eq!(val, *bytemuck::from_bytes(actual.val))
-    }
     
     #[test]
 	fn read_and_write_to_log() {
@@ -262,18 +241,12 @@ mod tests {
             .local_write(b"Hello, world!\n")
             .expect("failed to write to replica");
 
-		let mut read_buf: Vec<u8> = Vec::with_capacity(512);
+		let mut read_buf = EventBuf::<{size::EVENT_MAX}>::new();
 		replica.read(&mut read_buf, 0).expect("failed to read to file");
     
-        let actual_event_header: &EventHeader = 
-            bytemuck::from_bytes(&read_buf[0..EventHeader::LEN]);
+        let event = read_buf.as_event();
 
-        let actual_event = Event {
-            id: EventID { origin: actual_event_header.origin, pos: 0 },
-            val: &read_buf[EventID::LEN..]
-        };
-
-		assert_eq!(&actual_event.val, b"Hello, world!\n");
+		assert_eq!(&event.val, b"Hello, world!\n");
 		let path = replica.path.clone();
 		std::fs::remove_file(path).expect("failed to remove file");
     }
@@ -287,7 +260,7 @@ fn main() {
         .local_write(b"Who is this doin' this synthetic type of alpha beta psychedelic funkin'?")
         .expect("failed to write to replica");
 
-    let mut read_buf: Vec<u8> = Vec::with_capacity(512);
+    let mut read_buf = EventBuf::<{size::EVENT_MAX}>::new();
     replica.read(&mut read_buf, 0).expect("failed to read to file");
 
     for chunk in read_buf.chunks(8) {
