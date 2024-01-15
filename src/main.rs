7c382fbb213db2acc1b3952d5cda24e3a3ae5428
Lewis Campbell <info@lewiscampbell.tech>
2024-01-16 08:45:35 +1300

    using tempdir, so I no longer worry about creating directories

--- src/main.rs
+++ src/main.rs
@@ -168,7 +168,7 @@ struct EventIndex { pos: usize, len: usize }
 
 pub struct LocalReplica {
     pub id: ReplicaID,
-    pub path: String,
+    pub path: std::path::PathBuf,
     log_fd: fd::OwnedFd,
     log_len: usize,
     indices: Vec<EventIndex>,
@@ -177,47 +177,23 @@ pub struct LocalReplica {
 }
 
 impl LocalReplica {
+
     pub fn new<R: Rng>(
-        dir_path: &str, rng: &mut R
+        dir_path: &std::path::Path, rng: &mut R
     ) -> rustix::io::Result<Self> {
         let id = ReplicaID::new(rng);
-        let path = format!("{}/{}", dir_path, id);
-		let log_fd = Self::open_log(&path)?;
+
+        let path = dir_path.join(id.to_string());
+        let flags = O::DIRECT | O::CREATE | O::APPEND | O::RDWR | O::DSYNC;
+        let mode = fs::Mode::RUSR | fs::Mode::WUSR;
+		let log_fd = fs::open(&path, flags, mode)?;
+
 		let log_len = 0;
         let indices = vec![];
         let write_cache = EventBuf::new(2);
         let read_cache = EventBuf::new(2);
+
 		Ok(Self { id, path, log_fd, log_len, indices, write_cache, read_cache })
-    }
-
-    /*
-    // Open existing replica
-    pub fn open(path: &str) -> rustix::io::Result<Self> {
-		let log_fd = Self::open_log(&path)?;
-        let mut log_len: usize = 0;
-        let mut event_offset = 0;
-        let mut read_cache = EventBuf::new(2);
-
-        loop {
-            let bytes_read = 
-                io::pread(log_fd.as_fd(), &mut read_cache, event_offset)?;
-
-            log_len += bytes_read;
-
-            let event_header: &EventHeader = 
-                bytemuck::from_bytes(&read_cache[0..EventHeader::LEN]);
-        }
-
-        panic!("at the disco") 
-    }
-    */
-
-    fn open_log(path: &str) -> rustix::io::Result<fd::OwnedFd> {
-		fs::open(
-			path,
-			O::DIRECT | O::CREATE | O::APPEND | O::RDWR | O::DSYNC,
-			fs::Mode::RUSR | fs::Mode::WUSR,
-		)
     }
     
     // Event local to the replica, that don't yet have an ID
@@ -264,17 +240,19 @@ impl LocalReplica {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    println("TESTS STARTING");
+    use tempfile::{TempDir};
     
     #[test]
 	fn read_and_write_to_log() {
+        let tmp_dir = 
+            TempDir::with_prefix("interlog-").expect("failed to open temp file");
+
         let e1 = b"I've not grown weary on lenghty roads";
         let e2 = b"On strange paths, not gone astray";
         let e3 = b"Such is the knowledge, the knowledge cast in me";
         let e4 = b"Such is the knowledge; such are the skills";
 		let mut rng = rand::thread_rng();
-		let mut replica = LocalReplica::new("/tmp/interlog", &mut rng)
+		let mut replica = LocalReplica::new(tmp_dir.path(), &mut rng)
             .expect("failed to open file");
 		replica
             .local_write(&[e1, e2, e3, e4])
