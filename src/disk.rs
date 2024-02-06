55d34558958ea2ef0b0bf3313d2d0afa3a6a842f
Lewis Campbell <info@lewiscampbell.tech>
2024-02-07 10:02:21 +1300

    it's fine git don't ask me if I want to add the new file which is the only source of change in the repo into my new commit

---
+++ src/disk.rs
@@ -0,0 +1,37 @@
+use rustix::{fd, io};
+
+/*
+pub fn read_from_file(
+   bytes: &mut [u8], fd: fd::BorrowedFd, index: &Index
+) -> io::Result<()> {
+    // need to set len so pread knows how much to fill
+    if index.len > self.bytes.capacity() { panic!("OVERFLOW") }
+    unsafe {
+        self.bytes.set_len(index.len);
+    }
+
+    // pread ignores the fd offset, supply your own
+    let bytes_read = io::pread(fd, &mut self.bytes, index.pos as u64)?;
+    // If this isn't the case, we should figure out why!
+    assert_eq!(bytes_read, index.len);
+
+    Ok(())
+}
+*/
+#[derive(Debug, PartialEq)]
+pub enum Err {
+    OS(rustix::io::Errno),
+    NonAtomic {bytes_expected: usize, bytes_written: usize}
+}
+
+pub fn write(fd: fd::BorrowedFd, bytes: &[u8]) -> Result<usize, Err> {
+    // always sets file offset to EOF.
+    let bytes_written = io::write(fd, bytes).map_err(Err::OS)?;
+    // Linux 'man open': appending to file opened w/ O_APPEND is atomic
+    // TODO: will this happen? if so how to recover?
+    let bytes_expected = bytes.len();
+    if bytes_written != bytes_expected {
+        return Err(Err::NonAtomic{bytes_expected, bytes_written})
+    }
+    Ok(bytes_written)
+}
