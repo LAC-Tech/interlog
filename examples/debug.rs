8ba06caaf28a91f61c2623acdc59d80ae838baee
Lewis Campbell <info@lewiscampbell.tech>
2024-02-28 17:00:17 +1300

    fixed module exports

--- examples/debug.rs
+++ examples/debug.rs
@@ -10,20 +10,20 @@ fn main() {
         .expect("failed to open temp file");
 
     let mut rng = rand::thread_rng();
-    let config = Config {
+    let config = replica::Config {
         index_capacity: 16,
         read_cache_capacity: interlog::unit::Byte(256),
         txn_write_buf_capacity: interlog::unit::Byte(256)
     };
 
-    let mut replica = Local::new(tmp_dir.path(), &mut rng, config)
+    let mut replica = replica::Local::new(tmp_dir.path(), &mut rng, config)
         .expect("failed to open file");
 
     let vals = es.iter().map(Deref::deref);
     replica.local_write(vals)
         .expect("failed to write to replica");
 
-    let mut read_buf = FixVec::new(0x400);
+    let mut read_buf = utils::FixVec::new(0x400);
     replica.read(&mut read_buf, 0).expect("failed to read to file");
 
     let events: Vec<_> = read_buf.into_iter()
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 extern crate alloc;
 mod disk;
-mod event;
-mod replica;
-mod replica_id;
-pub mod unit;
-mod utils;
 
 mod test_utils;
+
+pub mod event;
+pub mod replica;
+pub mod replica_id;
+pub mod utils;
+pub mod unit;
+
 #[cfg(not(target_pointer_width = "64"))]
 compile_error!("code assumes usize is u64");
 
@@ -15,8 +17,3 @@ compile_error!("code assumes linux");
 
 #[cfg(not(target_endian = "little"))]
 compile_error!("code assumes little-endian");
-
-pub use replica::*;
-pub use replica_id::*;
-pub use utils::FixVec;
-pub use event::*;
