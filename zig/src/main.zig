2a9869cc4851d23a30a06cd8427d98130c427374
Lewis Campbell <info@lewiscampbell.tech>
2024-03-11 12:38:46 +1300

    putting all the helper data structures in their own module

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -2,126 +2,24 @@ const std = @import("std");
 const testing = std.testing;
 const Allocator = std.mem.Allocator;
 
-const mem = struct {
-    pub fn Region(comptime T: type) type {
-        return struct {
-            pos: usize,
-            len: usize,
+const util = @import("util.zig");
 
-            pub fn init(pos: usize, len: usize) @This() {
-                return @This(){ .pos = pos, .len = len };
-            }
-
-            pub fn zero() @This() {
-                return init(0, 0);
-            }
-
-            fn end(self: @This()) usize {
-                return self.pos + self.len;
-            }
-
-            pub fn lengthen(self: *@This(), len_diff: usize) void {
-                self.len += len_diff;
-                self.end = self.pos + self.len;
-            }
-
-            pub fn read(self: @This(), bytes: []const T) []const T {
-                return bytes[self.pos..self.end()];
-            }
-
-            pub fn write(self: *@This(), dest: []T, src: []const T) void {
-                @memcpy(dest[self.pos..self.end()], src);
-            }
-
-            pub fn empty(self: *@This()) bool {
-                return self.len == 0;
-            }
-        };
-    }
-};
-
-pub fn JaggedArray(comptime T: type) type {
-    return struct {
-        flat_mem: std.ArrayList(T),
-        indices: std.ArrayList(mem.Region(T)),
-        allocator: Allocator,
-
-        pub fn init(allocator: Allocator) !@This() {
-            return @This(){
-                .flat_mem = std.ArrayList(T).init(allocator),
-                .indices = std.ArrayList(mem.Region(u8)).init(allocator),
-                .allocator = allocator,
-            };
-        }
-
-        pub fn deinit(self: *@This()) void {
-            self.flat_mem.deinit();
-            self.indices.deinit();
-            self.* = undefined;
-        }
-
-        fn append(self: *@This(), items: []const T) Allocator.Error!void {
-            try self.indices.append(.{
-                .pos = self.flat_mem.items.len,
-                .len = items.len,
-            });
-            try self.flat_mem.appendSlice(items);
-        }
-
-        fn count(self: *@This()) usize {
-            return self.indices.items.len;
-        }
-
-        fn get(self: @This(), index: usize) []const T {
-            const region = self.indices.items[index];
-            return region.read(self.flat_mem.items);
-        }
-    };
-}
-
-test "messing around with jagged arrays" {
-    var ja = try JaggedArray(u8).init(testing.allocator);
-    defer ja.deinit();
-
-    try testing.expectEqual(ja.count(), 0);
-
-    try ja.append("9PM");
-    try testing.expectEqual(ja.count(), 1);
-    try testing.expectEqualSlices(u8, ja.get(0), "9PM");
-
-    try ja.append("Til");
-    try testing.expectEqual(ja.count(), 2);
-    try testing.expectEqualSlices(u8, ja.get(1), "Til");
-
-    try ja.append("I");
-    try testing.expectEqual(ja.count(), 3);
-    try testing.expectEqualSlices(u8, ja.get(2), "I");
-
-    try ja.append("Come");
-    try testing.expectEqual(ja.count(), 4);
-    try testing.expectEqualSlices(u8, ja.get(3), "Come");
-}
-
-const Buf = struct {
+const ReadCache = struct {
     buf: []u8,
-    a: mem.Region(u8),
-    b: mem.Region(u8),
+    a: util.Region(u8),
+    b: util.Region(u8),
 
     pub fn init(allocator: Allocator, capacity: usize) !@This() {
-        return Buf{
+        return ReadCache{
             .buf = try allocator.alloc(u8, capacity),
-            .a = mem.Region(u8).zero(),
-            .b = mem.Region(u8).zero(),
+            .a = util.Region(u8).zero(),
+            .b = util.Region(u8).zero(),
         };
     }
 
     pub fn deinit(self: *@This(), allocator: Allocator) void {
         allocator.free(self.buf);
         self.* = undefined;
-    }
-
-    pub fn append_slice(self: *@This()) void {
-        _ = self;
     }
 
     // TODO: delete below, just inline
@@ -135,7 +33,7 @@ const Buf = struct {
 };
 
 test "alloc and free buf" {
-    var buf = try Buf.init(testing.allocator, 8);
-    defer buf.deinit(testing.allocator);
-    try testing.expectEqualSlices(u8, buf.read_a(), &[_]u8{});
+    var rc = try ReadCache.init(testing.allocator, 8);
+    defer rc.deinit(testing.allocator);
+    try testing.expectEqualSlices(u8, rc.read_a(), &[_]u8{});
 }
---
+++ zig/src/util.zig
@@ -0,0 +1,131 @@
+const std = @import("std");
+const testing = std.testing;
+const Allocator = std.mem.Allocator;
+
+pub fn Region(comptime T: type) type {
+    return struct {
+        pos: usize,
+        len: usize,
+
+        pub fn init(pos: usize, len: usize) @This() {
+            return @This(){ .pos = pos, .len = len };
+        }
+
+        pub fn zero() @This() {
+            return init(0, 0);
+        }
+
+        fn end(self: @This()) usize {
+            return self.pos + self.len;
+        }
+
+        pub fn lengthen(self: *@This(), len_diff: usize) void {
+            self.len += len_diff;
+            self.end = self.pos + self.len;
+        }
+
+        pub fn read(self: @This(), bytes: []const T) []const T {
+            return bytes[self.pos..self.end()];
+        }
+
+        pub fn write(self: *@This(), dest: []T, src: []const T) void {
+            @memcpy(dest[self.pos..self.end()], src);
+        }
+
+        pub fn empty(self: *@This()) bool {
+            return self.len == 0;
+        }
+    };
+}
+
+pub fn JaggedArray(comptime T: type) type {
+    return struct {
+        flat_mem: std.ArrayList(T),
+        indices: std.ArrayList(Region(T)),
+        allocator: Allocator,
+
+        pub fn init(allocator: Allocator) !@This() {
+            return @This(){
+                .flat_mem = std.ArrayList(T).init(allocator),
+                .indices = std.ArrayList(Region(u8)).init(allocator),
+                .allocator = allocator,
+            };
+        }
+
+        pub fn deinit(self: *@This()) void {
+            self.flat_mem.deinit();
+            self.indices.deinit();
+            self.* = undefined;
+        }
+
+        fn append(self: *@This(), items: []const T) Allocator.Error!void {
+            try self.indices.append(.{
+                .pos = self.flat_mem.items.len,
+                .len = items.len,
+            });
+            try self.flat_mem.appendSlice(items);
+        }
+
+        fn count(self: *@This()) usize {
+            return self.indices.items.len;
+        }
+
+        fn get(self: @This(), index: usize) []const T {
+            const region = self.indices.items[index];
+            return region.read(self.flat_mem.items);
+        }
+    };
+}
+
+test "messing around with jagged arrays" {
+    var ja = try JaggedArray(u8).init(testing.allocator);
+    defer ja.deinit();
+
+    try testing.expectEqual(ja.count(), 0);
+
+    try ja.append("9PM");
+    try testing.expectEqual(ja.count(), 1);
+    try testing.expectEqualSlices(u8, ja.get(0), "9PM");
+
+    try ja.append("Til");
+    try testing.expectEqual(ja.count(), 2);
+    try testing.expectEqualSlices(u8, ja.get(1), "Til");
+
+    try ja.append("I");
+    try testing.expectEqual(ja.count(), 3);
+    try testing.expectEqualSlices(u8, ja.get(2), "I");
+
+    try ja.append("Come");
+    try testing.expectEqual(ja.count(), 4);
+    try testing.expectEqualSlices(u8, ja.get(3), "Come");
+}
+
+pub fn FixVec(comptime T: type) type {
+    return struct {
+        _items: []T,
+        len: usize,
+
+        pub fn init(allocator: Allocator, fixed_capacity: usize) !@This() {
+            return @This(){
+                ._items = try allocator.alloc(T, fixed_capacity),
+                .len = 0,
+            };
+        }
+
+        pub fn deinit(self: *@This(), allocator: Allocator) void {
+            allocator.free(self._items);
+            self.* = undefined;
+        }
+
+        pub fn capacity(self: *@This()) usize {
+            return self._items.len;
+        }
+    };
+}
+
+test "fixvec stuff" {
+    var fv = try FixVec(u64).init(testing.allocator, 8);
+    defer fv.deinit(testing.allocator);
+
+    try testing.expectEqual(fv.capacity(), 8);
+}
