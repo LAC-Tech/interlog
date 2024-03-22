70d07919e5b729d005fe6e591cc4e19436220f4c
Lewis Campbell <info@lewiscampbell.tech>
2024-03-22 20:52:56 +1300

    implemented replica cache logic, still need to test it

--- zig/src/main.zig
+++ zig/src/main.zig
@@ -77,18 +77,55 @@ const ReadCache = struct {
         self.* = undefined;
     }
 
+    pub fn update(self: @This(), txn_write_buf: []const u8) void {
+        const a_would_overflow = self.a.end + txn_write_buf.len + self.mem.len;
+
+        if (!a_would_overflow and self.b.end == 0) {
+            self.write_a(txn_write_buf);
+            return;
+        }
+
+        const a_will_be_modified = self.b.end + txn_write_buf.len >= self.a.pos;
+
+        if (!a_will_be_modified) {
+            self.write_b(txn_write_buf);
+            return;
+        }
+
+        const b_would_overflow = self.b.end + txn_write_buf.len > self.mem.len;
+
+        if (b_would_overflow) {
+            self.a = util.Region(u8).init(0, self.b.end);
+            self.b = util.Region(u8).zero();
+        }
+
+        if (self.new_a_byte_pos(txn_write_buf)) |new_a_pos| {
+            self.a.change_pos(new_a_pos);
+            self.write_b(txn_write_buf);
+        } else {
+            self.a = util.Region(u8).init(0, self.b.end);
+            self.b = util.Region(u8).zero();
+            self.write_a(txn_write_buf);
+        }
+    }
+
     fn set_logical_start(self: *@This(), es: []const u8) void {
         self.logical_start = event.read(es, 0).id.pos;
     }
 
     fn new_a_byte_pos(self: @This(), es: []const u8) ?usize {
         const new_b_end = self.b.end + es.len;
-        _ = new_b_end;
-
-        var result: ?usize = null;
-        _ = result;
 
         // return inside the loop once you find it, otherwise return null
+
+        var offset: usize = 0;
+
+        for (self.read_a()) |e| {
+            offset += e.onDiskSize();
+            if (offset > new_b_end) {
+                return offset;
+            }
+        }
 
         return null;
     }
@@ -129,23 +166,24 @@ test "alloc and free buf" {
     const replica_id = ReplicaID.init(&rng);
     _ = replica_id;
 
-    var ja: util.JaggedArray(Letter) = util.JaggedArray(Letter).init(testing.allocator);
+    var ja = util.JaggedArray(Letter).init(testing.allocator);
 
     defer ja.deinit();
-    try ja.append(&[_]Letter{ .w, .h, .o });
-    try ja.append(&[_]Letter{ .i, .s });
-    try ja.append(&[_]Letter{ .t, .h, .i, .s });
-    try ja.append(&[_]Letter{ .d, .o, .i, .n });
-    try ja.append(&[_]Letter{ .t, .h, .i, .s });
-    try ja.append(&[_]Letter{ .s, .y, .n, .t, .h, .e, .t, .i, .c });
-    try ja.append(&[_]Letter{ .t, .y, .p, .e });
-    try ja.append(&[_]Letter{ .o, .f });
-    try ja.append(&[_]Letter{ .a, .l, .p, .h, .a });
-    try ja.append(&[_]Letter{ .b, .e, .t, .a });
-    try ja.append(&[_]Letter{ .p, .s, .y, .c, .h, .e, .d, .e, .l, .i, .c });
-    try ja.append(&[_]Letter{ .f, .u, .n, .k, .i, .n });
+    try ja.append(&.{ .w, .h, .o });
+    try ja.append(&.{ .i, .s });
+    try ja.append(&.{ .t, .h, .i, .s });
+    try ja.append(&.{ .d, .o, .i, .n });
+    try ja.append(&.{ .t, .h, .i, .s });
+    try ja.append(&.{ .s, .y, .n, .t, .h, .e, .t, .i, .c });
+    try ja.append(&.{ .t, .y, .p, .e });
+    try ja.append(&.{ .o, .f });
+    try ja.append(&.{ .a, .l, .p, .h, .a });
+    try ja.append(&.{ .b, .e, .t, .a });
+    try ja.append(&.{ .p, .s, .y, .c, .h, .e, .d, .e, .l, .i, .c });
+    try ja.append(&.{ .f, .u, .n, .k, .i, .n });
 
     var rc = try ReadCache.init(testing.allocator, 8);
     defer rc.deinit(testing.allocator);
-    try testing.expectEqualSlices(u8, rc.read_a(), &[_]u8{});
+    try testing.expectEqualSlices(u8, rc.read_a(), &.{});
+    try testing.expectEqualSlices(u8, rc.read_b(), &.{});
 }
