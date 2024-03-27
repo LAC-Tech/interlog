37f0c4841e14320d6e686f4d8d03b32d4e764a51
Lewis Campbell <info@lewiscampbell.tech>
2024-03-28 12:48:56 +1300

    moving region into its own file and getting rid of the pointless util file. also cleaning up code we don't need anymore in log

--- src/event.rs
+++ src/event.rs
@@ -2,7 +2,7 @@
 use crate::fixvec;
 use crate::fixvec::FixVec;
 use crate::log_id::LogID;
-use crate::util::region::Region;
+use crate::region::Region;
 
 /// This ID is globally unique.
 /// TODO: is it worth trying to fit this into, say 128 bits? 80 bit replica ID,
---
+++ src/region.rs
@@ -0,0 +1,63 @@
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Region {
+	pub pos: usize,
+	pub len: usize
+}
+
+#[derive(Debug)]
+pub struct WriteErr;
+
+impl Region {
+	pub fn new(pos: usize, len: usize) -> Self {
+		Self { pos, len }
+	}
+
+	pub const ZERO: Self = Self { pos: 0, len: 0 };
+
+	pub fn lengthen(&mut self, n: usize) {
+		self.len += n;
+	}
+
+	pub fn end(&self) -> usize {
+		self.pos + self.len
+	}
+
+	/// Set pos to new_pos, while leaving the end the same
+	pub fn change_pos(&mut self, new_pos: usize) {
+		self.pos = new_pos;
+		self.len = self.end() - new_pos;
+	}
+
+	pub fn next(&self, len: usize) -> Self {
+		Self::new(self.end(), len)
+	}
+
+	pub fn read<'a>(&self, bytes: &'a [u8]) -> Option<&'a [u8]> {
+		bytes.get(self.pos..self.end())
+	}
+
+	pub fn write(&self, dest: &mut [u8], src: &[u8]) -> Result<(), WriteErr> {
+		// I tried using_copy_from slice, but it panics on overflow
+		// I need an error, so I basically reimplemented it
+		if self.end() > dest.len() {
+			return Err(WriteErr);
+		}
+
+		let dest = &mut dest[self.pos..self.end()];
+
+		// We know that src and dest are the same length
+		unsafe {
+			core::ptr::copy_nonoverlapping(
+				src.as_ptr(),
+				dest.as_mut_ptr(),
+				src.len()
+			);
+		}
+
+		Ok(())
+	}
+
+	pub fn empty(&self) -> bool {
+		self.len == 0
+	}
+}
