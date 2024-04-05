da468ac90c6226082a4e1577ebd3cfddd6a5e3b2
Lewis Campbell <info@lewiscampbell.tech>
2024-04-05 21:55:40 +1300

    dialog box for appending an event that does nothing

--- inspector/src/main.rs
+++ inspector/src/main.rs
@@ -1,5 +1,6 @@
 use cursive::theme::{BorderStyle, Palette, Theme};
-use cursive::views::{Dialog, ListView, SelectView, TextView};
+use cursive::traits::{Nameable, Resizable};
+use cursive::views::{Dialog, ListView, SelectView, TextArea};
 
 // Compact text representation of bytes to single braille characters
 fn u8_to_braille(n: u8) -> char {
@@ -32,9 +33,19 @@ fn main() {
 		palette: Palette::terminal_default(),
 	});
 
+	// q(uit)
 	siv.add_global_callback('q', |s| s.quit());
+	// a(ppend)
+	siv.add_global_callback('a', |s| {
+		let dialog = Dialog::new()
+			.title("Add an event")
+			.content(TextArea::new().fixed_width(50))
+			.button("Enqueue & Commit", |_| {});
+		s.add_layer(dialog);
+	});
 
-	siv.add_layer(SelectView::new().item("0", "item").item("1", "item"));
+	let event_view = SelectView::new().item("0", "item").item("1", "item");
+	siv.add_layer(event_view);
 
 	// Starts the event loop.
 	siv.run();
