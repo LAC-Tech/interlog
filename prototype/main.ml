1c6519517be2ebac82b3a23a55ad49dc958f55cd
Lewis Campbell <info@lewiscampbell.tech>
2024-06-08 12:35:10 +1200

    ready to start writing the simulation. need to think of a scenario though. or just see that SEC holds??

--- prototype/main.ml
+++ prototype/main.ml
@@ -13,43 +13,57 @@ type msg =
 module Index : sig
   type t
   val create : unit -> t
-  val add_seq : t -> Event.id Seq.t -> unit
+  val add_addrs : t -> addr Seq.t -> unit
+  val add_ids : t -> Event.id Seq.t -> unit
 end = struct
   type t = (addr, offset Dynarray.t) Hashtbl.t 
 
   let create () = Hashtbl.create 128
 
-  let add_seq index ids = 
+  let add_addrs index =
+    let add_addr addr = if not (Hashtbl.mem index addr) then 
+      Hashtbl.add index addr (Dynarray.create ()) 
+    in
+    Seq.iter add_addr 
+
+  let add_ids index = 
     let add_id (id: Event.id) = match Hashtbl.find_opt index id.origin with
     | Some offsets -> Dynarray.add_last offsets id.pos
     | None -> Hashtbl.add index id.origin (Dynarray.of_array [|id.pos|])
     in
-    Seq.iter add_id ids
+    Seq.iter add_id 
 end
 
 module Actor : sig
   type t
   val create : addr -> t
+  val add_acquaintances : t -> addr Seq.t -> unit
+  val addr : t -> addr
   val recv : t -> msg -> unit
 end = struct
-  type t = { log: Event.t Dynarray.t; index: Index.t }
+  type t = { addr: addr; log: Event.t Dynarray.t; index: Index.t }
 
-  let create addr = { log = Dynarray.create (); index = Index.create () } 
+  let create addr = {
+    addr = addr; 
+    log = Dynarray.create (); 
+    index = Index.create () 
+  } 
+
+  let add_acquaintances actor = Index.add_addrs actor.index
+
+  let addr actor = actor.addr
 
   let recv actor = function
     | Sync {events; from} -> 
-        let ids = 
-          events |> Array.to_seq |> Seq.map (fun (e: Event.t) -> e.id)
-        in
-        Index.add_seq actor.index ids;
+        let ids = events |> Array.to_seq |> Seq.map (fun e -> e.Event.id) in
+        Index.add_ids actor.index ids;
         Dynarray.append_array actor.log events
 end
 
-
 module Network : sig
   val send : msg -> addr -> unit
-  val spawn : unit -> addr
-  val inspect : addr -> Actor.t
+  val spawn : unit -> Actor.t
+  val find : addr -> Actor.t
 end = struct
   let actors = Hashtbl.create 128
 
@@ -62,13 +76,12 @@ end = struct
     if Hashtbl.mem actors addr then
       raise (Invalid_argument (Printf.sprintf "duplicate address %d" addr))
     else (
-      Hashtbl.add actors addr (Actor.create addr);
-      addr
+      let actor = Actor.create addr in
+      Hashtbl.add actors addr actor; 
+      actor
     )
 
-  let inspect = Hashtbl.find actors
-
-
+  let find = Hashtbl.find actors
 end
 
 let rng = Random.self_init ();;
