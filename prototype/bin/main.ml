c40d71f52e9c4ee35defff261530da2fc63abb4c
Lewis Campbell <info@lewiscampbell.tech>
2024-06-08 14:34:54 +1200

    set up proper dune project

--- prototype/bin/main.ml
+++ prototype/bin/main.ml
@@ -1,60 +1,57 @@
-type addr = int;;
-type offset = int;;
+type addr = int
+type offset = int
 
 module Event = struct
-  type id = {origin: addr; pos: int}
+  type id = { origin : addr; pos : int }
   type payload = string
-  type t = { id: id; payload: payload }
+  type t = { id : id; payload : payload }
 end
 
-type msg =
-  | Sync of {events: Event.t array; from: addr }
+type msg = Sync of Event.t array
 
 module Index : sig
   type t
+
   val create : unit -> t
   val add_addrs : t -> addr Seq.t -> unit
   val add_ids : t -> Event.id Seq.t -> unit
 end = struct
-  type t = (addr, offset Dynarray.t) Hashtbl.t 
+  type t = (addr, offset Dynarray.t) Hashtbl.t
 
   let create () = Hashtbl.create 128
 
   let add_addrs index =
-    let add_addr addr = if not (Hashtbl.mem index addr) then 
-      Hashtbl.add index addr (Dynarray.create ()) 
+    let add_addr addr =
+      if not (Hashtbl.mem index addr) then
+        Hashtbl.add index addr (Dynarray.create ())
     in
-    Seq.iter add_addr 
+    Seq.iter add_addr
 
-  let add_ids index = 
-    let add_id (id: Event.id) = match Hashtbl.find_opt index id.origin with
-    | Some offsets -> Dynarray.add_last offsets id.pos
-    | None -> Hashtbl.add index id.origin (Dynarray.of_array [|id.pos|])
+  let add_ids index =
+    let add_id (id : Event.id) =
+      match Hashtbl.find_opt index id.origin with
+      | Some offsets -> Dynarray.add_last offsets id.pos
+      | None -> Hashtbl.add index id.origin (Dynarray.of_array [| id.pos |])
     in
-    Seq.iter add_id 
+    Seq.iter add_id
 end
 
 module Actor : sig
   type t
+
   val create : addr -> t
   val add_acquaintances : t -> addr Seq.t -> unit
   val addr : t -> addr
   val recv : t -> msg -> unit
 end = struct
-  type t = { addr: addr; log: Event.t Dynarray.t; index: Index.t }
+  type t = { addr : addr; log : Event.t Dynarray.t; index : Index.t }
 
-  let create addr = {
-    addr = addr; 
-    log = Dynarray.create (); 
-    index = Index.create () 
-  } 
-
+  let create addr = { addr; log = Dynarray.create (); index = Index.create () }
   let add_acquaintances actor = Index.add_addrs actor.index
-
   let addr actor = actor.addr
 
   let recv actor = function
-    | Sync {events; from} -> 
+    | Sync events ->
         let ids = events |> Array.to_seq |> Seq.map (fun e -> e.Event.id) in
         Index.add_ids actor.index ids;
         Dynarray.append_array actor.log events
@@ -67,21 +64,24 @@ module Network : sig
 end = struct
   let actors = Hashtbl.create 128
 
-  let send msg addr = match Hashtbl.find_opt actors addr with
-  | Some a -> Actor.recv a msg
-  | None -> Printf.eprintf "no such address %d" addr
+  let send msg addr =
+    match Hashtbl.find_opt actors addr with
+    | Some a -> Actor.recv a msg
+    | None -> Printf.eprintf "no such address %d" addr
 
-  let spawn () = 
+  let spawn () =
     let addr = Random.full_int Int.max_int in
     if Hashtbl.mem actors addr then
       raise (Invalid_argument (Printf.sprintf "duplicate address %d" addr))
-    else (
+    else
       let actor = Actor.create addr in
-      Hashtbl.add actors addr actor; 
+      Hashtbl.add actors addr actor;
       actor
-    )
 
   let find = Hashtbl.find actors
 end
 
-let rng = Random.self_init ();;
+let () =
+  let seed = int_of_float (Unix.time ()) in
+  Random.init seed;
+  Printf.printf "Seed: %d\n" seed
