c4f5741f7953e3c4a6c748175d18d98db608d7e6
Lewis Campbell <info@lewiscampbell.tech>
2024-06-16 15:17:45 +1200

    defining what storage, log, and an event buffer should look like

--- core/src/actor.rs
+++ core/src/actor.rs
@@ -1,10 +1,38 @@
 use crate::event;
 use crate::fixed_capacity::Vec;
 use crate::index::Index;
+use crate::mem;
 use crate::pervasives::*;
 
-struct Actor {
+struct Actor<L: Log> {
 	addr: Addr,
-	//log: Vec<event::Event>,
+	storage: Storage<L>,
 	index: Index,
+}
+
+impl<L: Log> Actor<L> {
+	fn recv(msg: Msg) {
+		panic!("TODO")
+	}
+}
+
+enum Msg {
+	SyncResponse(),
+}
+
+struct Storage<L: Log> {
+	log: L,
+	txn_buffer: Vec<mem::Word>,
+}
+
+trait Log {}
+
+struct InMemLog<const MAX_EVENTS: usize, const MAX_WORDS: usize> {
+	indices: alloc::boxed::Box<[DiskOffset; MAX_EVENTS]>,
+	mem: alloc::boxed::Box<[mem::Word; MAX_EVENTS]>,
+}
+
+impl<const MAX_EVENTS: usize, const MAX_WORDS: usize> Log
+	for InMemLog<MAX_EVENTS, MAX_WORDS>
+{
 }
--- core/src/event.rs
+++ core/src/event.rs
@@ -50,6 +50,7 @@ pub const HEADER_SIZE: usize = core::mem::size_of::<Header>();
 #[derive(Clone, Debug)]
 pub struct Event<'a> {
 	pub id: ID,
+	/// Pointer because single event payload points to some block of memory
 	pub payload: &'a [mem::Word],
 }
 
@@ -86,23 +87,16 @@ pub fn append(buf: &mut Vec<mem::Word>, event: &Event) -> fixed_capacity::Res {
 	Ok(())
 }
 
-/*
-#[derive(bytemuck::Pod, bytemuck::Zeroable)]
-#[repr(C)]
-pub struct Batch<const N_INDICES: usize, const N_BYTES: usize> {
-	indices: [DiskOffset; N_INDICES],
-	bytes: [u8; N_BYTES],
-}
-*/
-
+// Event Iterator
+// Didn't just call it "Iterator" because then it would conflict
 pub struct View<'a> {
-	bytes: &'a [mem::Word],
-	byte_index: usize,
+	words: &'a [mem::Word],
+	index: usize,
 }
 
 impl<'a> View<'a> {
-	pub fn new(bytes: &'a [mem::Word]) -> Self {
-		Self { bytes, byte_index: 0 }
+	pub fn new(words: &'a [mem::Word]) -> Self {
+		Self { words, index: 0 }
 	}
 }
 
@@ -110,9 +104,28 @@ impl<'a> Iterator for View<'a> {
 	type Item = Event<'a>;
 
 	fn next(&mut self) -> Option<Self::Item> {
-		let result = read(self.bytes, self.byte_index)?;
-		self.byte_index += result.on_disk_size();
+		let result = read(self.words, self.index)?;
+		self.index += result.on_disk_size();
 		Some(result)
+	}
+}
+
+struct Buf(Vec<mem::Word>);
+
+impl Buf {
+	fn push(&mut self, event: &Event) -> fixed_capacity::Res {
+		append(&mut self.0, event)
+	}
+}
+
+struct Batch(alloc::boxed::Box<[mem::Word]>);
+
+impl<'a> IntoIterator for &'a Batch {
+	type Item = Event<'a>;
+	type IntoIter = View<'a>;
+
+	fn into_iter(self) -> Self::IntoIter {
+		View::new(&self.0)
 	}
 }
 
