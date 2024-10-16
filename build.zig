582ded93c660d160bcd0f862b7f78f6e50d223ad
Lewis Campbell <info@lewiscampbell.tech>
2024-10-17 11:38:48 +1300

    add ncurses to sim exe

--- build.zig
+++ build.zig
@@ -36,6 +36,8 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    exe.linkSystemLibrary("ncurses");
+
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
